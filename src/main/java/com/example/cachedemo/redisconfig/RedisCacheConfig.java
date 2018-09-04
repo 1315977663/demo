64588756848bcfa5a8b6a4efec1a4960b5f879f2
fbl <1315977663@qq.com>
2018-09-04 18:01:00 +0800

    配置Redis缓存

---
+++ src/main/java/com/example/cachedemo/redisconfig/RedisCacheConfig.java
@@ -0,0 +1,97 @@
+package com.example.cachedemo.redisconfig;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.lang.reflect.Method;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @program: cachedemo
+ * @description: redis缓存配置
+ * @author: fbl
+ * @create: 2018-09-04 09:50
+ **/
+@Configuration
+@ConfigurationProperties(prefix = "myredis")
+@Slf4j
+@Data
+public class RedisCacheConfig {
+
+    private List<CacheInfo> cache;
+
+    @Bean
+    public KeyGenerator myGenerator() {
+        return (Object o, Method method, Object... objects) -> {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append(o.getClass().getSimpleName());
+            stringBuilder.append(method.getName());
+            stringBuilder.append("(");
+            for (Object object : objects) {
+                stringBuilder.append(objects.toString());
+            }
+            stringBuilder.append(")");
+            String key = stringBuilder.toString();
+            log.info("【缓存key】{}", key);
+            return key;
+        };
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        return new RedisCacheManager(
+                RedisCacheWriter.nonLockingRedisCacheWriter(redisConnectionFactory),
+                this.getRedisCacheConfigurationWithTtl(200),
+                this.getRedisCacheConfigurationMap()
+        );
+    }
+
+    private Map<String, RedisCacheConfiguration> getRedisCacheConfigurationMap() {
+        Map<String, RedisCacheConfiguration> map = new HashMap<>();
+        log.info("{}", cache);
+        for (CacheInfo info : cache) {
+            map.put(info.name, this.getRedisCacheConfigurationWithTtl(info.time));
+        }
+        return map;
+    }
+
+    private RedisCacheConfiguration getRedisCacheConfigurationWithTtl(Integer seconds) {
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<Object>(Object.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig();
+        redisCacheConfiguration = redisCacheConfiguration.serializeValuesWith(
+                RedisSerializationContext
+                    .SerializationPair
+                    .fromSerializer(jackson2JsonRedisSerializer)
+        ).entryTtl(Duration.ofSeconds(seconds));
+
+        return redisCacheConfiguration;
+    }
+}
+
+@Data
+class CacheInfo{
+    String name;
+    Integer time;
+}
