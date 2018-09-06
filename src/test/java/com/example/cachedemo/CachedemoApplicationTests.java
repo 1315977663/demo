4343b76089502c68c58005ac4870e95c329094d1
fbl <1315977663@qq.com>
2018-09-07 00:11:47 +0800

    test io Stream

--- src/test/java/com/example/cachedemo/CachedemoApplicationTests.java
+++ src/test/java/com/example/cachedemo/CachedemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.cachedemo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,8 +8,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.*;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class CachedemoApplicationTests {
 
     @Autowired
@@ -22,5 +26,44 @@ public class CachedemoApplicationTests {
     @Cacheable(value = "product", keyGenerator = "myGenerator")
     public String setCache(String value) {
         return value;
+    }
+
+
+    public static void main (String arg[]) {
+        try {
+            liu();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void liu() throws IOException {
+        BufferedReader bufferedReader = null;
+        BufferedWriter bufferedWriter = null;
+        try {
+            bufferedReader = new BufferedReader(
+                    new InputStreamReader(
+                            System.in
+                    )
+            );
+            bufferedWriter = new BufferedWriter(
+                    new OutputStreamWriter(
+                            new FileOutputStream("D:/bbb.txt")
+                    )
+            );
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        String a = null;
+        while(!(a = bufferedReader.readLine()).equalsIgnoreCase("exit")) {
+           log.info("{}", a);
+            assert bufferedWriter != null;
+            bufferedWriter.write(a);
+            bufferedWriter.write("\r\n");
+        }
+        assert bufferedWriter != null;
+        bufferedWriter.flush();
+        bufferedWriter.close();
+        bufferedReader.close();
     }
 }
